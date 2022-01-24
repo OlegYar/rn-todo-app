7d98b7b5f62906097c0b38b3cba5818be4171104
Oleg Yarotskiy <aleh.yarotski@awtg.co.uk>
2022-01-24 18:33:28 +0300

    Added navbar and todo form

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Navbar from "./src/Navbar";
+import TodoForm from "./src/TodoForm";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title={'Todo App'}/>
+      <View style={styles.container}>
+          <TodoForm/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      paddingHorizontal: 30,
+      paddingVertical: 20,
   },
 });
